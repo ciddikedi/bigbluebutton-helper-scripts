30d6b8e457935b4bc55d3aba0df1a69b90d88f90
Osman Bedrettin Yüce <bedrettin.yuce@ogr.sakarya.edu.tr>
2021-04-22 00:52:03 +0300

    yeni script ve açıklama

---
+++ delete-transfered-meetings-age.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MAXAGE=15
+shopt -s nullglob
+NOW=$(date +%s)
+
+for donefile in /var/bigbluebutton/recording/scalelite/copied/* ; do
+        MTIME=$(stat -c %Y "${donefile}")
+        if [ $(( ( $NOW - $MTIME ) / 86400 )) -gt $MAXAGE ]; then
+                MEETING_ID=$(basename "${donefile}")
+                rm -rf /var/bigbluebutton/published/presentation/${donefile}
+        fi
+done
