147de45f60b72bb88e1116e983c4c9d39921552e
Osman Bedrettin Yüce <bedrettin.yuce@ogr.sakarya.edu.tr>
2021-04-22 16:22:17 +0300

    hata düzeltme

--- delete-transfered-meetings-age.sh
+++ delete-transfered-meetings-age.sh
@@ -8,6 +8,6 @@ for donefile in /var/bigbluebutton/recording/scalelite/copied/* ; do
         MTIME=$(stat -c %Y "${donefile}")
         if [ $(( ( $NOW - $MTIME ) / 86400 )) -gt $MAXAGE ]; then
                 MEETING_ID=$(basename "${donefile}")
-                rm -rf /var/bigbluebutton/published/presentation/${donefile}
+                rm -rf /var/bigbluebutton/published/presentation/${MEETING_ID}
         fi
 done
