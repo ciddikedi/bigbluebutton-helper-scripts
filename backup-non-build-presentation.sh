854472ab92ca0e2c142aa6d9135bffdd251d16f2
Osman Bedrettin Yüce <bedrettin.yuce@ogr.sakarya.edu.tr>
2021-04-26 12:28:08 +0300

    StrictHostKeyChecking düzenlemesi

--- backup-non-build-presentation.sh
+++ backup-non-build-presentation.sh
@@ -3,6 +3,6 @@ mainserver=XXXXXX
 bbb-record --list | grep "^.\{128\} " | cut -b 1-54 >> list.txt
 filename='list.txt'
 while read p; do 
-  rsync -a /var/bigbluebutton/recording/raw/${p} root@${mainserver}:/mnt/scalelite-recordings/var/bigbluebutton/backup-raw
+  rsync  -e "ssh -o StrictHostKeyChecking=no" -a /var/bigbluebutton/recording/raw/${p} root@${mainserver}:/mnt/scalelite-recordings/var/bigbluebutton/backup-raw
 done < $filename
 rm list.txt
