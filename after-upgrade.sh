7cf32557ab5876a099d011bb80dc1a584b312eda
Osman Bedrettin Yüce <bedrettin.yuce@ogr.sakarya.edu.tr>
2021-07-10 11:19:53 +0300

    iç-dış ip değişiklikleri

--- after-upgrade.sh
+++ after-upgrade.sh
@@ -3,8 +3,8 @@ IP=$(curl -s http://whatismyip.akamai.com)
 TARGET1=/opt/freeswitch/conf/sip_profiles/external.xml
 TARGET2=/usr/local/bigbluebutton/bbb-webrtc-sfu/config/default.yml
 
-xmlstarlet edit --inplace --update '//param[@name="local_ip_v4"]/@value' --value "\$\${external_rtp_ip}" $TARGET1
-xmlstarlet edit --inplace --update '//param[@name="local_ip_v4"]/@value' --value "\$\${external_sip_ip}" $TARGET1
+xmlstarlet edit --inplace --update '//param[@name="ext-rtp-ip"]/@value' --value "\$\${external_rtp_ip}" $TARGET1
+xmlstarlet edit --inplace --update '//param[@name="ext-sip-ip"]/@value' --value "\$\${external_sip_ip}" $TARGET1
 yq w -i $TARGET2 kurento[0].ip "$IP"
 yq w -i $TARGET2 freeswitch.ip "$IP"
 yq w -i $TARGET2 freeswitch.sip_ip "$IP"
